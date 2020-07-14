3584186c1103211fa61fee853f2f57113b91bbbb
Anukasrh Gupta <agupta3@kloudrac.com>
2020-07-14 22:19:35 +0530

    'feedback'

--- routes/procurement.js
+++ routes/procurement.js
@@ -37,10 +37,10 @@ router.get('/assetEditDetails',(request, response) =>{
     let assetId = request.query.assetId;
     console.log('assetId  '+assetId);
 
-    let qyr='SELECT asset.id, asset.sfid,asset.name as name ,asset.Activity_Code__c, asset.GST__c,asset.Requested_Closure_Plan_Date__c,asset.Requested_Closure_Actual_Date__c,asset.Project_Department__c, '+
+    let qyr='SELECT asset.id, asset.sfid as sfid,asset.name as name ,asset.Activity_Code__c, asset.GST__c,asset.Requested_Closure_Plan_Date__c,asset.Requested_Closure_Actual_Date__c,asset.Project_Department__c, '+
     'asset.Manager_Approval__c,asset.Management_Approval__c,asset.Procurement_Committee_Approval__c,asset.Chairperson_Approval__c,asset.Committee_Approved_Counts__c,'+
     'asset.Comittee_Rejected_Count__c,asset.Procurement_Committee_Status__c,asset.Accounts_Approval__c,asset.Procurement_Head_Approval__c,asset.Approval_Status__c,'+
-    'asset.Number_Of_IT_Product__c,asset.Number_Of_Non_IT_Product__c,asset.Procurement_IT_total_amount__c,asset.Procurement_Non_IT_total_amount__c, asset.Total_amount__c,proj.name as projname,proj.sfid, '+
+    'asset.Number_Of_IT_Product__c,asset.Number_Of_Non_IT_Product__c,asset.Procurement_IT_total_amount__c,asset.Procurement_Non_IT_total_amount__c, asset.Total_amount__c,proj.name as projname,proj.sfid as profsfid, '+
     'asset.Management_Approval_Activity_Code__c,asset.Management_Approval_for_fortnight_limit__c, '+
     'asset.Management_Approval_less_than_3_quotes__c,asset.Procurement_Comt_Approval_for_fortnight__c, '+
      'asset.P_O_attachment__c,payment_status__c,asset.status__c,asset.payment_received_acknowledgement__c,asset.receiver_name__c,asset.received_quantity_goods__c,asset.date_of_receiving_goods__c '+
@@ -56,7 +56,7 @@ router.get('/assetEditDetails',(request, response) =>{
        .then((assetQueryResult)=> {
            if(assetQueryResult.rowCount > 0)
            {
-               console.log('assetQueryResult  '+assetQueryResult.rows);
+               console.log('assetQueryResult EDIT '+JSON.stringify(assetQueryResult.rows));
                response.send(assetQueryResult.rows[0]);
            }
            else
@@ -84,7 +84,7 @@ router.get('/details',verify, async(request, response) => {
  'asset.Number_Of_IT_Product__c,asset.Number_Of_Non_IT_Product__c,asset.Procurement_IT_total_amount__c,asset.Procurement_Non_IT_total_amount__c, asset.Total_amount__c,proj.name as projname,proj.sfid, '+
  'asset.Management_Approval_Activity_Code__c,asset.Management_Approval_for_fortnight_limit__c, '+
  'asset.Management_Approval_less_than_3_quotes__c,asset.Procurement_Comt_Approval_for_fortnight__c, '+
-  'asset.P_O_attachment__c,payment_status__c,asset.status__c,asset.payment_received_acknowledgement__c,asset.receiver_name__c,asset.received_quantity_goods__c,asset.date_of_receiving_goods__c '+
+  'asset.P_O_attachment__c,asset.payment_status__c,asset.Status__c,asset.Payment_Received_Acknowledgement__c,asset.receiver_name__c,asset.received_quantity_goods__c,asset.date_of_receiving_goods__c '+
  'FROM  salesforce.Asset_Requisition_Form__c asset '+
   'INNER JOIN salesforce.Milestone1_Project__c proj '+
   'ON asset.Project_Department__c =  proj.sfid '+
@@ -96,7 +96,7 @@ router.get('/details',verify, async(request, response) => {
     .then((assetQueryResult)=> {
         if(assetQueryResult.rowCount > 0)
         {
-            console.log('assetQueryResult  '+assetQueryResult.rows);
+            console.log('assetQueryResult  '+JSON.stringify(assetQueryResult.rows));
             assetFormAndRelatedRecords.assetFormDetails = assetQueryResult.rows;        
         }
         else
@@ -237,14 +237,23 @@ router.post('/updateasset',(request,response)=>{
     console.log('goodsDate  '+goodsDate);
     console.log('assetid  '+assetid);
     let updateQuerry = 'UPDATE salesforce.Asset_Requisition_Form__c SET '+
-    'category__c = \''+cate+'\', '+
-    'items__c = \''+item+'\', '+
-    'unit__c = \''+unit+'\', '+
-    'per_unit_cost__c = \''+cost+'\', '+
-    'Other_Items__c= \''+other+'\' '+
+    'Name = \''+assetName+'\', '+
+    'Requested_Closure_Actual_Date__c = \''+closureActualDate+'\', '+
+    'Requested_Closure_Plan_Date__c = \''+closurePlanDate+'\', '+
+    'Activity_Code__c = \''+activityCode+'\', '+
+    'Payment_Status__c = \''+paymentStatus+'\', '+
+    'Status__c = \''+status+'\', '+
+    'Payment_Received_Acknowledgement__c = \''+payement+'\', '+
+    'Receiver_Name__c = \''+receiverName+'\', '+
+    'Received_Quantity_Goods__c= \''+receivedQuantity+'\', '+
+    'Date_of_Receiving_Goods__c= \''+goodsDate+'\' '+
     'WHERE sfid = $1';
     console.log('updateQuerry '+updateQuerry);
-    response.send('okoko');
+   pool.query(updateQuerry,[assetid])
+   .then((queryResultUpdate)=>{
+       console.log('queryResultUpdate '+JSON.stringify(queryResultUpdate));
+       response.send('succesfully inserted');
+   }).catch((eroor)=>{console.log(JSON.stringify(eroor.stack))})
 })
 
 
@@ -426,8 +435,6 @@ router.get('/itProducts/:parentAssetId',verify, (request,response) => {
     response.render('procurementIT',{name: request.user.name, email: request.user.email, parentAssetId: parentAssetId});
 
 });
-
-
 router.post('/itProducts', (request,response) => {
 
     console.log('Inside ItProducts Post Method');
@@ -812,9 +819,10 @@ router.get('/NonItProcurementList',(request,response)=>{
 
 
 })
-router.get('/getProcurementDetail',(request,response)=>{
+router.get('/getProcurementDetail',async(request,response)=>{
     let procurementId=request.query.procurementId;
     console.log('getProcurementITDetail Id='+procurementId);
+    let procDetail={};
     let qry='SELECT proc.sfid,proc.Name as procName ,proc.Items__c ,proc.Others__c,proc.Approvers__c,proc.Products_Services_Name__c,vend.name as vendorName,proc.Product_Service__c,proc.Quantity__c, proc.Budget__c,proc.Impaneled_Vendor__c, '+
     'proc.State__c,proc.District__c,proc.Quote1__c,proc.Quote2__c,proc.Quote3__c,proc.Per_Unit_Cost__c,proc.unit__c,proc.Number_of_quotes__c,proc.justification__c '+
     'FROM salesforce.Product_Line_Item__c proc '+
@@ -825,12 +833,24 @@ router.get('/getProcurementDetail',(request,response)=>{
     .query(qry,[procurementId])
     .then((querryResult)=>{
     console.log('QuerryResult=>'+JSON.stringify(querryResult.rows));
-    response.send(querryResult.rows);
-})
-.catch((querryError)=>{
-    console.log('QuerrError '+querryError.stack);
-    response.send(querryError);
-})
+   // response.send(querryResult.rows);
+    procDetail.proc=querryResult.rows;
+    console.log('oddskd '+JSON.stringify(procDetail));
+    pool.query('SELECT sfid,Name,Timely_submissions_of_all_Deliverables__c,Work_Quality_Goods_Quality__c,Issue_Knowledge_Expertise__c,Procurement_Non_IT__c FROM salesforce.Feedback__c WHERE Procurement_Non_IT__c=$1',[procurementId])
+                .then((queryResult)=>{
+                         console.log('queryResult'+JSON.stringify(queryResult));
+                         procDetail.feedback=queryResult.rows;
+                         console.log('procDetail list :'+JSON.stringify(procDetail));
+                         response.send(procDetail);
+                })
+                .catch((error)=>{
+                    console.log('erroror '+JSON.stringify(error.stack));
+                })
+                .catch((querryError)=>{
+                console.log('QuerrError '+querryError.stack);
+                response.send(querryError);
+                })
+    })
 })
 router.post('/updateProcurement',(request,response)=>{
     let body = request.body;
@@ -1249,4 +1269,80 @@ response.send('Error');
 
 
 })
+
+router.get('/createFeedback/:procid',verify,(request,response)=>{
+    let procid=request.params.procid;
+    let objUser=request.user;
+    console.log('obhUser =>'+objUser);
+    console.log('parentId '+procid);
+    response.render('createFeedbackform',{procid,objUser});
+
+})
+router.get('/getfeedback/:procid',verify,(request,response)=>{
+    let procid=request.params.procid;
+    let objUser=request.user;
+    console.log('obhUser =>'+objUser);
+    console.log('parentId '+procid);
+    response.render('getlistFeedback',{procid,objUser});
+
+})
+router.get('/getFeedbacklist',verify,(request,response)=>{
+    let parentid=request.query.parentId;
+    console.log('parentid '+parentid);
+    let qry = 'SELECT sfid,Name,Timely_submissions_of_all_Deliverables__c,Work_Quality_Goods_Quality__c,Issue_Knowledge_Expertise__c,Procurement_Non_IT__c FROM salesforce.Feedback__c WHERE Procurement_Non_IT__c=$1';
+    console.log('qry  =>'+qry)
+     pool.query(qry,[parentid])
+     .then((feedbackqueryresult) => {
+         console.log('feedbackqueryresult  : '+JSON.stringify(feedbackqueryresult.rows));
+         if(feedbackqueryresult.rowCount>0){
+            let modifiedList = [],i =1;
+             feedbackqueryresult.rows.forEach((eachRecord) => {
+              let obj = {};
+              obj.sequence = i;
+              obj.name = '<a href="#" class="vendorTag" id="'+eachRecord.sfid+'" >'+eachRecord.name+'</a>';
+              obj.time=eachRecord.timely_submissions_of_all_deliverables__c;
+              obj.quality=eachRecord.work_quality_goods_quality__c;
+              obj.issue = eachRecord.issue_knowledge_expertise__c;
+              obj.editAction = '<button href="#" class="btn btn-primary editVendor" id="'+eachRecord.sfid+'" >Edit</button>'
+              i= i+1;
+              modifiedList.push(obj);
+            })
+            console.log('modifiedList '+JSON.stringify(modifiedList));
+            response.send(modifiedList);
+        }
+        else
+        {
+            response.send([]);
+        }
+     })
+     .catch((error) => {
+         console.log('error  : '+error.stack);
+         response.send('Error Occurred !');
+     })
+})
+
+
+router.post('/savefeedback',(request,response)=>{
+    let body = request.body;
+    console.log('body  : '+JSON.stringify(body));
+    const{time,quality, issue,procidt}=request.body;
+    console.log('time'+time);
+    console.log('quality'+quality);
+    console.log('procidt'+procidt);
+    console.log('issue'+issue);
+
+    let feedCreateqry = 'INSERT INTO salesforce.Feedback__c (Timely_submissions_of_all_Deliverables__c,Work_Quality_Goods_Quality__c,Procurement_Non_IT__c,Issue_Knowledge_Expertise__c ) VALUES ($1,$2,$3,$4)';
+    console.log('feedCreateqry=>'+feedCreateqry);
+    pool.query(feedCreateqry,[time,quality,procidt,issue])
+    .then((queryResult)=>{
+        console.log('feedback INsert query result '+JSON.stringify(queryResult));
+        response.send('succesfully inserted');
+    })
+    .catch((error)=>{
+        console.log(error.stack);
+        response.send(error);
+    })
+})
+
+
 module.exports = router;
